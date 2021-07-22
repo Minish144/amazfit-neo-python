c0419d003f869325d888952af27ad2be028e1fdd
= <=>
2021-07-22 16:49:12 +0300

    auth added

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # amazfit-neo-python
-# amazfit-neo-python
+
+## Installing dependencies
+```bash
+$ python3 -m venv env
+
+$ source env/bin/activate
+
+$ pip install -r requirements.txt
+```
+
+## Setting MAC address and auth key
+set `MAC_AMAZFIT_NEO` and `KEY` in main.py
+
+## Running
+```bash
+python3 main.py
+```
--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import threading
 import binascii
 import time
 import struct
+import sys
 
 # Device MAC address
 MAC_AMAZFIT_NEO = 'C1:CC:A3:0A:B1:94'
+
+# Auth key
+KEY = b'\xa7\x89\x95\xf2\x03\x88\xcbo-\xd7\xbbF\xa2R\x10I'
 
 # Anhui Huami service
 UUID_SVC_HUAMI = '0000fee0-0000-1000-8000-00805f9b34fb'
@@ -15,21 +19,25 @@ UUID_CHAR_BATTERY = '00000006-0000-3512-2118-0009af100700'
 # Anhui Huami auth service
 UUID_SVC_HUAMI_AUTH = '0000fee1-0000-1000-8000-00805f9b34fb'
 UUID_CHAR_AUTH = '00000009-0000-3512-2118-0009af100700'
-# UUID_DESC_AUTH = '00002902-0000-1000-8000-00805f9b34fb'
+UUID_DESC_AUTH = '00002902-0000-1000-8000-00805f9b34fb'
 
 # Heart Rate service
 UUID_SVC_HEART_RATE = '0000180d-0000-10008000-00805f9b34fb'
 UUID_CHAR_HRM_MEASURE = '00002a37-0000-1000-8000-00805f9b34fb'
 UUID_CHAR_HRM_CONTROL = '00002a39-0000-1000-8000-00805f9b34fb'
 
-# CCCD_UUID = 0x2902
+CCCD_UUID = 0x2902
 
-KEY = b'\xa7\x89\x95\xf2\x03\x88\xcbo-\xd7\xbbF\xa2R\x10I'
+class Utils(object):
+    @staticmethod
+    def encrypt(key, message):
+        aes = AES.new(key, AES.MODE_ECB)
+        return aes.encrypt(message)
 
 class AmazfitNeo(Peripheral):
     __notifications_thread: threading.Thread
 
-    def __init__(self, MAC: str):
+    def __init__(self, MAC: str) -> Peripheral:
         super().__init__(MAC)
 
     def inspect(self) -> None:
@@ -44,14 +52,17 @@ class AmazfitNeo(Peripheral):
     def read_battery(self) -> any:
         char = self.get_battery_char()
         if char.supportsRead():
-            return char.read()
-        else:
-            raise Exception('char is not readable')
+            bytes = char.read()
+            return struct.unpack('b', bytes[1:2])[0] if len(bytes) >= 2 else None
+        return None
+        
 
     def auth(self) -> None:
-        self.getServiceByUUID(UUID_SVC_HUAMI).getCharacteristics('00000020-0000-3512-2118-0009af100700')[0].write(b'\x01\x00')
-        self.get_auth_char().write(b'\x01\x00')
-        # self.get_auth_desc().write(b'\x01\x00', True)
+        self.get_auth_desc().write(b"\x01\x00", True)
+        self.waitForNotifications(0.5)
+
+        self.get_auth_char().write(struct.pack('<2s', b'\x02\x00'))
+        self.waitForNotifications(0.5)
 
     def start_heartrate(self) -> None:
         self.get_heartrate_control_char().write(b'\x15\x01\x01', True)
@@ -107,10 +118,9 @@ class AmazfitNeo(Peripheral):
         self.__notifications_thread.start()
 
     def __start_notifications_listening_thread(self) -> None:
-        # self.setDelegate(NotificationDelegate(self))
         t = threading.currentThread()
         while getattr(t, 'do_run', True):
-            self.waitForNotifications(1.0)
+            self.waitForNotifications(0.5)
 
 class NotificationDelegate(DefaultDelegate):
     def __init__(self, device: AmazfitNeo):
@@ -118,13 +128,31 @@ class NotificationDelegate(DefaultDelegate):
         self.device = device
 
     def handleNotification(self, hnd, data):
-        print(f'[{hnd}] {data}')
         if hnd == self.device.get_auth_char().getHandle():
-            print(self.device.get_auth_char().getHandle())
             if data[:3] == b'\x10\x01\x01':
-                print(f'Received {data[:3]}')
-                self.device.get_auth_char().write(b'\x02\x08')
-                self.device.waitForNotifications(1)
+                self.device.get_auth_char().write(struct.pack('<2s', b'\x02\x00'))
+                self.device.waitForNotifications(0.5)
+            
+            elif data[:3] == b'\x10\x01\x04':
+                print('Sending key failed')
+
+            elif data[:3] == b'\x10\x02\x01':
+                cmd = struct.pack('<2s', b'\x03\x00') + Utils().encrypt(KEY, data[3:])
+                send_cmd = struct.pack('<18s', cmd)
+                self.device.get_auth_char().write(send_cmd)
+                self.device.waitForNotifications(0.5)
+
+            elif data[:3] == b'\x10\x02\x04':
+                print('Requesting random key error')
+
+            elif data[:3] == b'\x10\x03\x04':
+                print('Ecnryption key failed!')
+
+            elif data[:3] == b'\x10\x03\x01':
+                print('Auth completed!')
+            
+            else:
+                print('Auth failed')
 
         elif hnd == self.device.get_heartrate_control_measure_char().getHandle():
             self.__handle_heartrate_notification(hnd, data)
@@ -135,33 +163,25 @@ class NotificationDelegate(DefaultDelegate):
         rate = struct.unpack('bb', data)[1]
         print('Heart Rate: ', str(rate))
 
-def addrToBytes(addr):
-    return binascii.unhexlify(addr.replace(b':', b''))
-
-def encrypt(self, message):
-    aes = AES.new(self._KEY, AES.MODE_ECB)
-    return aes.encrypt(message)
-
 def main():
     print('Starting...')
 
     band = AmazfitNeo(MAC_AMAZFIT_NEO) # getting band class
     print('Connected successfully!')
 
-    band.setDelegate(NotificationDelegate(band))
+    notificationHandler = NotificationDelegate(band)
+    band.setDelegate(notificationHandler)
 
     band.auth()
-    band.waitForNotifications(5)
 
-    band.get_auth_char().write(struct.pack('<18s', b'\x01\x08' + KEY))
-    band.waitForNotifications(5)
-    band.waitForNotifications(5)
-    band.waitForNotifications(5)
-
-    band.get_auth_char().write(struct.pack('<2s', b'\x02\x08'))
-    band.waitForNotifications(5)
-    band.waitForNotifications(5)
-    band.waitForNotifications(5)
+    timestart = 0
+    while True:
+        print(timestart)
+        print(band.read_battery())
+        time.sleep(10)
+        timestart += 10
 
 if __name__ == '__main__':
     main()
+
+    
